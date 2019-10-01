307399204ccd5efd8db0c01f8360bf726d5b9d6e
PedABR <48610012+PedABR@users.noreply.github.com>
2019-10-01 09:43:31 -0300

    Update Shor's Algorithm.py
    Achei que o estilo do código fica mais consistente dessa maneira

--- Shor's Algorithm.py
+++ Shor's Algorithm.py
@@ -1,25 +1,27 @@
 from random import randrange
 from math import floor
 
-def euclid_algo(x, y):
-
-	if x < y:
-		return euclid_algo(y, x)
-
-	while y != 0:
-		(x, y) = (y, x % y)
-
+def euclid_algo(num1, num2):
+	
+	if num1 < num2:
+		return euclid_algo(num2, num1)
+	
+	while num2 != 0:
+		num1, num2 = num2, num1 % num2
+		
 	return x
 
 def verificaIgualdade(num, lista):
+	
 	for number in lista:
 		if number == num:
 			return True
-
 	return False
 
 def contaFrequencia(number, lista):
+	
 	count = 0
+	
 	for num in lista:
 		count += 1
 		if num == number:
@@ -28,30 +30,28 @@ def contaFrequencia(number, lista):
 def descobrirFrequencia(rand, num, grandeza = 15):
 	
 	restosLista = []
+	
 	for x in range(grandeza):
-		resto = (rand**x)%num
+		resto = (rand**x) % num
 		if not verificaIgualdade(resto, restosLista):
 			restosLista.append(resto)
-
 		else:
 			return contaFrequencia(resto, restosLista)
-
 	return 0
 
 def decodificar(num, grandeza):
+	
 	freq = 0
 	factors = []
-
-	while freq == 0 or not (freq%2 == 0):
+	
+	while freq == 0 or not (freq % 2 == 0):
 		rand = randrange(2, num)
 		freq = descobrirFrequencia(rand, num)
-
-
+		
 	guess = rand**(freq/2)
 
 	factors.append(euclid_algo(guess+1,num))
 	factors.append(euclid_algo(guess-1,num))
-
 
 	if factors[0] == 1:
 		factors[0] = num/factors[1]
@@ -64,13 +64,11 @@ def decodificar(num, grandeza):
 	
 	return factors
 
-
 def main():
+	
 	num = int(input("The chosen number: "))
 	grandeza = int(input("Number of trys: "))
 	print(decodificar(num, grandeza))
-
-
 
 if __name__ == '__main__':
 	main()
